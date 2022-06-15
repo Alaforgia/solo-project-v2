a92e764e72fb7fd5d60f8a740cae8b4e88d3585e
Tony LaForgia <alforgia428@gmail.com>
2022-06-15 17:41:09 -0500

    pool.js bug fix

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -8,9 +8,9 @@
 const pg = require('pg');
 const url = require('url');
 
-​let config = {};
+let config = {};
 
-​// We need a different pg configuration if we're running
+// We need a different pg configuration if we're running
 // on Heroku, vs if we're running locally.
 //
 // Heroku gives us a process.env.DATABASE_URL variable,
@@ -30,20 +30,20 @@ if (process.env.DATABASE_URL) {
     database: 'recipe_app', // CHANGE THIS LINE to match your local database name!
   };
 }
-​
+
 // this creates the pool that will be shared by all other modules
 const pool = new pg.Pool(config);
-​
+
 // the pool will log when it connects to the database
 pool.on('connect', () => {
   console.log('Postgesql connected');
 });
-​
+
 // the pool with emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
 pool.on('error', (err) => {
   console.log('Unexpected error on idle client', err);
   process.exit(-1);
 });
-​
+
 module.exports = pool;
